60367e6b2f4c2be68105e07ca96c47f401f5e428
Bruno Windels <bruno.windels@gmail.com>
2012-02-12 15:32:07 +0100

    make drawing code more modular, so we can better experiment with different algorithms

--- graph.js
+++ graph.js
@@ -1,125 +1,130 @@
 var PathFindr = PathFindr || {};
-PathFindr.drawMap = (function() {
-    var canvas, options, path, grid;
+PathFindr.SquareGridCanvas = (function() {
     
-    function tilePixelPos(x, y) {
-        return {
-            x: x * (options.pixelsPerTile + options.borderWidth) + options.borderWidth/2,
-            y: y * (options.pixelsPerTile + options.borderWidth) + options.borderWidth/2
-        };
+    function SquareGridCanvas(grid, canvas, options) {
+        this.grid = grid;
+        this.canvas = canvas;
+        this.options = options;
     }
     
-    function drawGrid(ctx) {
-        var w, y;
-        ctx.strokeStyle = '#804000';
-        ctx.lineWidth = options.borderWidth;
-        ctx.beginPath();
-        for(w = 0; w < canvas.width; w+= options.pixelsPerTile + options.borderWidth) {
-            ctx.moveTo(w, 0);
-            ctx.lineTo(w, canvas.height);
-        }
-        for(h = 0; h < canvas.height; h+= options.pixelsPerTile + options.borderWidth) {
-            ctx.moveTo(0, h);
-            ctx.lineTo(canvas.width, h);
-        }
-        ctx.stroke();
-    }
-    
-    function drawTiles(ctx) {
-        var x, y, color, pos;
-        for(x = 0; x < grid.width; ++x) {
-            for(y = 0; y < grid.height; ++y) {
-                color = getTileColor(x, y);
-                if(color) {
-                    ctx.fillStyle = color;
-                    pos = tilePixelPos(x, y);
-                    ctx.fillRect(
-                        pos.x,
-                        pos.y,
-                        options.pixelsPerTile + options.borderWidth,
-                        options.pixelsPerTile + options.borderWidth
-                    );
+    SquareGridCanvas.prototype = {
+        tilePos: function(x, y) {
+            return {
+                x: x * this.options.pixelsPerTile,
+                y: y * this.options.pixelsPerTile
+            };
+        },
+        tileCenter: function(x, y) {
+            return {
+                x: x * this.options.pixelsPerTile + this.options.pixelsPerTile / 2,
+                y: y * this.options.pixelsPerTile + this.options.pixelsPerTile / 2
+            };
+        },
+        getContext: function() {
+            if(!this.ctx) {
+                this.canvas.height = this.grid.height * this.options.pixelsPerTile;
+                this.canvas.width = this.grid.width * this.options.pixelsPerTile;
+                this.ctx = this.canvas.getContext('2d');
+            }
+            return this.ctx;
+        },
+        drawGrid: function() {
+            var ctx = this.getContext(), w, y;
+            ctx.strokeStyle = '#804000';
+            ctx.lineWidth = this.options.borderWidth;
+            ctx.beginPath();
+            for(w = 0; w < this.canvas.width; w+= this.options.pixelsPerTile) {
+                ctx.moveTo(w - this.options.borderWidth/2, 0);
+                ctx.lineTo(w - this.options.borderWidth/2, this.canvas.height);
+            }
+            for(h = 0; h < this.canvas.height; h+= this.options.pixelsPerTile) {
+                ctx.moveTo(0, h - this.options.borderWidth/2);
+                ctx.lineTo(this.canvas.width, h - this.options.borderWidth/2);
+            }
+            ctx.stroke();
+        },
+        drawTiles: function() {
+            var ctx = this.getContext(), x, y, color, pos;
+            for(x = 0; x < this.grid.width; ++x) {
+                for(y = 0; y < this.grid.height; ++y) {
+                    color = this.getTileColor(x, y);
+                    if(color) {
+                        ctx.fillStyle = color;
+                        pos = this.tilePos(x, y);
+                        ctx.fillRect(
+                            pos.x,
+                            pos.y,
+                            this.options.pixelsPerTile,
+                            this.options.pixelsPerTile
+                        );
+                    }
                 }
             }
-        }
-    }
-    
-    function getTileColor(x, y) {
-        if(x === path.startNode.x && y === path.startNode.y) {
-            return '#6dd34f';
-        }
-        if(x === path.endNode.x && y === path.endNode.y) {
-            return '#214210';
-        }
-        if(grid.isObstacle(x, y)) {
-            return '#804000';
-        }
-        return null;
-    }
-    
-    function drawPath(ctx, path) {
-        function drawPathNode(node, i) {
-            var pos = tilePixelPos(node.x, node.y);
-            var center = {
-                x: pos.x + options.pixelsPerTile/2,
-                y: pos.y + options.pixelsPerTile/2
-            };
-            ctx.lineTo(center.x, center.y);
-        }
-        ctx.strokeStyle = 'orange';
-        ctx.lineWidth = options.borderWidth * 8;
-        ctx.beginPath();
-        path.nodes().forEach(drawPathNode);
-        ctx.stroke();
-    }
-    
-    function drawOpenAndClosedList(ctx) {
-        function drawNode(color, node) {
-            var pos = tilePixelPos(node.x, node.y);
-            var center = {
-                x: pos.x + options.pixelsPerTile/2,
-                y: pos.y + options.pixelsPerTile/2
-            };
-            var parentOffset = node.parentOffset();
-            var pointerPos = {
-                x: center.x + ((options.pixelsPerTile/2) * parentOffset.x),
-                y: center.y + ((options.pixelsPerTile/2) * parentOffset.y)
-            };
-            var radius = options.pixelsPerTile / 4;
-            ctx.fillStyle = color;
+        },
+        getTileColor: function(x, y) {
+            if(this.grid.isObstacle(x, y)) {
+                return '#804000';
+            }
+            return null;
+        },
+        drawPath: function(path, color) {
+            var ctx = this.getContext();
+            ctx.strokeStyle = ctx.fillStyle = color;
+            ctx.lineWidth = this.options.borderWidth * 8;
             ctx.beginPath();
-            ctx.arc(center.x, center.y, radius, 0, Math.PI*2, true);
-            ctx.fill();
-            ctx.lineWidth = options.borderWidth*2;
-            ctx.strokeStyle = color;
-            ctx.beginPath();
-            ctx.moveTo(center.x, center.y);
-            ctx.lineTo(pointerPos.x , pointerPos.y);
+            path.nodes().forEach(function(node) {
+                var center = this.tileCenter(node.x, node.y);
+                ctx.lineTo(center.x, center.y);
+            },this);
             ctx.stroke();
+            var endPoints = [
+                this.tileCenter(path.startNode.x, path.startNode.y),
+                this.tileCenter(path.endNode.x, path.endNode.y)
+            ];
+            var radius = this.options.pixelsPerTile / 2.2;
+            endPoints.forEach(function(center) {
+                ctx.beginPath();
+                ctx.arc(center.x, center.y, radius, 0, Math.PI*2, true);
+                ctx.fill();
+            });
+            
+            if(this.options.debug) {
+                this.drawPathDebugInfo(path);
+            }
+        },
+        drawPathDebugInfo: function(path) {
+            var ctx = this.getContext();
+            function drawNode(color, node) {
+                var center = this.tileCenter(node.x, node.y);
+                var parentOffset = node.parentOffset();
+                var pointerPos = {
+                    x: center.x + ((this.options.pixelsPerTile/2) * parentOffset.x),
+                    y: center.y + ((this.options.pixelsPerTile/2) * parentOffset.y)
+                };
+                var radius = this.options.pixelsPerTile / 4;
+                ctx.fillStyle = color;
+                ctx.beginPath();
+                ctx.arc(center.x, center.y, radius, 0, Math.PI*2, true);
+                ctx.fill();
+                ctx.lineWidth = this.options.borderWidth*2;
+                ctx.strokeStyle = color;
+                ctx.beginPath();
+                ctx.moveTo(center.x, center.y);
+                ctx.lineTo(pointerPos.x , pointerPos.y);
+                ctx.stroke();
+            }
+            path.openList.forEach(drawNode.bind(this, '#1514ff'));
+            path.closedList.forEach(drawNode.bind(this, '#c800ff'));
+        },
+        clear: function() {
+            var ctx = this.getContext();
+            ctx.fillStyle = 'white';
+            ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        },
+        draw: function() {
+            this.drawTiles();
+            this.drawGrid();
         }
-        path.openList.forEach(drawNode.bind(null, '#1514ff'));
-        path.closedList.forEach(drawNode.bind(null, '#c800ff'));
-    }
-    
-    function drawMap(g, p, c, o) {
-        options = o;
-        canvas = c;
-        grid = g;
-        path = p;
-        path.nodes();
-        canvas.height = grid.height * options.pixelsPerTile + grid.height + options.borderWidth;
-        canvas.width = grid.width * options.pixelsPerTile + grid.width + options.borderWidth;
-        var ctx = canvas.getContext('2d');
-        ctx.fillStyle = 'white';
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-        
-        drawTiles(ctx);
-        drawPath(ctx, path);
-        
-        if(options.debug) {
-            drawOpenAndClosedList(ctx);
-        }
-        drawGrid(ctx);
-    }
-    return drawMap;
+    };
+    return SquareGridCanvas;
 })();
