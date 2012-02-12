3951836110a589033e15041e45cb8fa838191ac9
Bruno Windels <bruno.windels@gmail.com>
2012-02-12 17:52:00 +0100

    correctly calculate all rectangles in the nav mesh

--- aanavmesh.js
+++ aanavmesh.js
@@ -16,54 +16,55 @@ PathFindr.AANavMesh = (function() {
         }
         
         function findRectangle(x, y) {
-            var w = 1, h = 1, curX, curY;
+            var w = 0, h = 0, curX, curY;
             var rightObstacleFound = false,
                 bottomObstacleFound = false;
             
             while(!rightObstacleFound || !bottomObstacleFound) {
+                //increase this dimension to a new 'experimental' value and see if we bump into something
+                if(!rightObstacleFound) {
+                    ++w;
+                }
                 //try expand width
                 if(!rightObstacleFound) {
                     curX = x + w - 1;
                     for(curY = y; curY <= y + h - 1; ++curY) {
                         if(!grid.isPassable(curX, curY) || containsRectangle(curX, curY)) {
-                            console.log('width expander bumped into something at',curX, curY);
                             rightObstacleFound = true;
-                            --w;
+                            --w; //we bumped into something, go back one step so we can keep looking in the other direction
                             break;
                         }
                     }
+                }
+                //increase this dimension to a new 'experimental' value and see if we bump into something
+                if(!bottomObstacleFound) {
+                    ++h;
                 }
                 //try expand height
                 if(!bottomObstacleFound) {
                     curY = y + h - 1;
                     for(curX = x; curX <= x + w - 1; ++curX) {
                         if(!grid.isPassable(curX, curY) || containsRectangle(curX, curY)) {
-                            console.log('height expander bumped into something at',curX, curY);
                             bottomObstacleFound = true;
-                            --h;
+                            --h; //we bumped into something, go back one step so we can keep looking in the other direction
                             break;
                         }
                     }
                 }
-                if(!rightObstacleFound) {
-                    ++w;
-                }
-                if(!bottomObstacleFound) {
-                    ++h;
-                }
+                //if we stepped back to 0, the start position is not passable or already has a rect
                 if(w === 0 || h === 0) {
                     return null;
                 }
             }
             
             var r = new PathFindr.Rectangle(x, y, w, h);
-            console.log(r.x1+','+r.y1+': '+r.width()+'x'+r.height()+': '+r.area());
             return r;
         }
         var rect, x, y;
         
-        for(x = 0; x < grid.width; ++x) {
             for(y = 0; y < grid.height; ++y) {
+                for(x = 0; x < grid.width; ++x) {
+                
                 rect = findRectangle(x, y);
                 if(rect) {
                     rects.push(rect);
