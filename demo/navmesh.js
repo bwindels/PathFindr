9c23846fea30c398718d1de0a4abc4a11ba5da98
Bruno Windels <bruno.windels@gmail.com>
2013-03-25 15:08:47 +0100

    adjustments to navmesh demo

--- demo/navmesh.js
+++ demo/navmesh.js
@@ -5,7 +5,7 @@ var Path = require('../lib/astar');
 var SquareGridCanvas = require('./graph');
 var NavMesh = require('../lib/navmesh');
 
-module.exports = function(canvas, stats) {
+module.exports = function(canvas) {
     var options = {
         pixelsPerTile: 20,
         borderWidth: 1,
@@ -17,11 +17,11 @@ module.exports = function(canvas, stats) {
     var map = new SquareGridCanvas(grid, canvas, options);
 
     var startNode = mesh.node(1, 2);
-    var endNode = mesh.node(1, 10);
-    var start = new Date().getTime();
+    var endNode = mesh.node(4, 10);
     var path = new Path(startNode, endNode, options);
-
+    var coordinates = mesh.finishPath(path.nodes(), {x: 4, y: 10});
     map.drawNavMesh(mesh);
-    //map.drawPath(path, 'purple');
+    map.drawDots(coordinates, 'purple');
+    map.drawCoordinates(coordinates, 'yellow');
     map.draw();
 };
